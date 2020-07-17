f6bc13075fe42957d81681f2dbfda5f3d60959f2
Bernardo Henz <bernardohenz@gmail.com>
2020-07-17 11:56:57 -0300

    updated bash for downloading links

--- datasets/download_datasets.sh
+++ datasets/download_datasets.sh
@@ -2,10 +2,14 @@
 
 cd datasets/
 
-wget -N http://inf.ufrgs.br/~bhenz/datasets.zip
+fileid="1lnFhEnEv6rAF8apVj5issvA9lDxXq8T1"
+filename="datasets.zip"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
 
 unzip datasets.zip
 
 rm -rf datasets.zip
+rm -rf cookie
 
 cd ..
--- trained_models/download_trained_models.sh
+++ trained_models/download_trained_models.sh
@@ -2,10 +2,17 @@
 
 cd trained_models/
 
-wget -N http://inf.ufrgs.br/~bhenz/trained_models.zip
+
+fileid="1IamcP_p058wC7XwrvyWOfwoWjSPV94_t"
+filename="trained_models.zip"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
+#wget -N http://inf.ufrgs.br/~bhenz/trained_models.zip
 
 unzip trained_models.zip
 
 rm -rf trained_models.zip
+rm -rf cookie
 
 cd ..
